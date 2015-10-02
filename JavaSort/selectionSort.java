5d5a3aff4d574669ae296a674f352832fce2a729
GurevichAlexander <gurevich.alexander95@gmail.com>
2015-10-02 18:36:29 +0300

    JavaSort

---
+++ JavaSort/selectionSort.java
@@ -0,0 +1,37 @@
+package javasort;
+
+
+public class selectionSort{
+    private static void printArray(double[] array){
+        for (int i = 0; i < array.length; ++i)
+        {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+    public static void selectionSort(double[] num){
+        double temp;
+        for (int i = num.length - 1; i > 0; i--){
+            int first = 0;
+            for (int j = 1; j <= i; ++j) {
+                if (num[j] > num[first])
+                    first = j;
+            }
+            temp = num[first];
+            num[first] = num[i];
+            num[i] = temp;
+        }
+    }
+
+    public static void main(String[] args){
+        double[] array = new double[args.length];
+        for (int i = 0; i < args.length; ++i){
+            array[i] = Double.parseDouble(args[i]);
+        }
+        selectionSort(array);
+        printArray(array);
+    }
+
+}
