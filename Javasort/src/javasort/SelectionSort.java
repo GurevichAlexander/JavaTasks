6bd4a4d1876ebabd59b5c121691d3d458e660d31
GurevichAlexander <gurevich.alexander95@gmail.com>
2015-10-16 19:31:12 +0300

    3rd attempt :(

--- Javasort/src/javasort/SelectionSort.java
+++ Javasort/src/javasort/SelectionSort.java
@@ -1,19 +1,18 @@
 package javasort;
 
 
-public class selectionSort{
-    private static void printArray(double[] array){
-        for (int i = 0; i < array.length; ++i)
-        {
+public class SelectionSort {
+    private static void printArray(double[] array) {
+        for (int i = 0; i < array.length; ++i) {
             System.out.print(array[i] + " ");
         }
         System.out.println();
     }
 
 
-    public static void selectionSort(double[] num){
+    public static void selectionSort(double[] num) {
         double temp;
-        for (int i = num.length - 1; i > 0; i--){
+        for (int i = num.length - 1; i > 0; i--) {
             int first = 0;
             for (int j = 1; j <= i; ++j) {
                 if (num[j] > num[first])
@@ -25,9 +24,9 @@ public class selectionSort{
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         double[] array = new double[args.length];
-        for (int i = 0; i < args.length; ++i){
+        for (int i = 0; i < args.length; ++i) {
             array[i] = Double.parseDouble(args[i]);
         }
         selectionSort(array);
